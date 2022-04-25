2d6e60b9ebeacadc61093ac49c64559e6cab82ee
wdelacruzl <wdelacruzl98@gmail.com>
2022-04-25 18:28:22 -0400

    initial commit

---
+++ src/main/java/main.java
@@ -0,0 +1,7 @@
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Hello, this is practice =). honestly i'm okay with this part. " +
+                "the part that i forgot to do earlier was the copying to my CMD I forgot it had to" +
+                "be done there and did it here instead... oops ");
+    }
+}
