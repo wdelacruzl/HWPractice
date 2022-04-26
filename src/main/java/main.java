d31c6443e721bc19cc4839c5bac43a72fab8fbd1
wdelacruzl <wdelacruzl98@gmail.com>
2022-04-25 20:33:53 -0400

    practice

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -3,5 +3,20 @@ public class main {
         System.out.println("Hello, this is practice =). honestly i'm okay with this part. " +
                 "the part that i forgot to do earlier was the copying to my CMD I forgot it had to" +
                 "be done there and did it here instead... oops ");
+
+        // Practice
+        // While loop
+//        while(true){   // THIS LOOP NEVER STOPS because this condition will always be true
+//            System.out.println("Hello World");
+//        int a = 89;
+//        while (a<100) {  // while this loop is less than 100 a increases by 1
+//            System.out.println(a);
+//            a++;
+//        }
+//        System.out.println("loop finished");
+        //FOR LOOP
+        for (int a = 0; a < 3; a++) { // this is the simpler way of doing what we did ^^^
+            System.out.println(a);
+        }
     }
 }
